fc2d4b95a26a17dfadc88910bd9bc6b97b84e70d
chukxsemmy <chukxsemmy@gmail.com>
2023-10-18 02:00:15 +0000

    returns all students sorted by avg score

---
+++ 101-students.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Top Students
+"""
+
+
+def top_students(mongo_collection):
+    """returns all students sorted by avg score
+    """
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
